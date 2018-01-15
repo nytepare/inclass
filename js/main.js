be41c3635d61160c4040582cfdebf96cd3d5e254
ambernytepare <ambernytepare@gmail.com>
2018-01-15 14:03:09 -0500

    almost there

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    // If you see ready in the console log of the browser, you have connected the js file correctly.
+
+    // https://www.w3schools.com/jquery/event_click.asp
+});
+
